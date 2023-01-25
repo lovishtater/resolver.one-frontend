21e8acdbb6d0de16176b850c1cc8cf375868d08e
Saurabh Chidrewar <saurabhchidrewar5@gmail.com>
2023-01-25 13:18:01 +0530

    Added already have an account option to signup and vice versa

--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,2 @@
-// export const API = "https://resolver-one-backend.vercel.app";
-export const API = "http://localhost:8000";
+export const API = "https://resolver-one-backend.vercel.app";
+// export const API = "http://localhost:8000";
